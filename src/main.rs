ab00f53b30f6f5e0c10ef01ffdfcb5bed5340f0c
Nora Breitmoser-Widdecke <nora.breitmoser-widdecke@dlr.de>
2024-02-07 14:27:41 +0100

    feat: draft data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+#![allow(dead_code)]
+
+type Id = String;
+
+struct Objective {
+    id: Id,
+    name: String,         // 1.1 (name is not quite right)
+    description: String,  // "The activities of the software life cycle processes are defined.""
+    reference: Reference, //  4.1.a (Chapter in the Standard)
+    //applicability: Applicability, // not here, we already know which DAL level we need
+    satisfied: ObjectiveState,
+}
+
+struct Reference {
+    document: String,
+    isbn: Option<()>,
+    reference: String, // chapter in standard
+}
+
+enum ObjectiveState {
+    Failed,
+    Satisfied,
+    // We want to be stateless, thus an objective is always either failed or satisfied.
+    // Pending
+}
+
+struct Evidence {
+    objective: Id,
+    uri: url::Url,
+    kind: EvidenceKind,
+}
+
+// determine how the associated evidence is interpreted
+enum EvidenceKind {
+    Justification,
+    CoverageReport,
+    FileJustExists,
+}
+
+// struct Applicability {
+//     dal_a: ApplicabilityType,
+//     dal_b: ApplicabilityType,
+//     dal_c: ApplicabilityType,
+//     dal_d: ApplicabilityType,
+// }
+
+// enum ApplicabilityType {
+//     NotApplicable,
+//     Applicable,
+//     ApplicableWithIndependence,
+// }
+
 fn main() {
     println!("Hello, world!");
 }
