e28c658dfccc8ba78db7cc7320fc6ad1713b0c5e
Tim Schubert <dadada@dadada.li>
2024-07-25 17:14:50 +0200

    feat: add requirements coverage info to junit output

--- tests/test_conv_junit.py
+++ tests/test_conv_junit.py
@@ -1,13 +1,13 @@
-import io  # noqa: F401
-
 from coua.input import load_junit_xml
 from importlib import resources
-from rdflib import Graph, URIRef
+from rdflib import Graph, URIRef, Literal
 from . import res
 
 
 class TestConvJunit:
-    def test_all_subjects_recognized(self):
+    def test_all_subjects_recognized(self, record_property):
+        record_property("requirement", "Req21")
+
         xml = resources.files(res).joinpath("valid_junit.xml")
         graph = Graph()
         load_junit_xml(graph, xml)
@@ -15,3 +15,14 @@ class TestConvJunit:
         subjects = list(graph.subjects())
         for i in range(1, 10):
             assert URIRef(f"https://llg.cubic.org/docs/junit#testCase{i}") in subjects
+
+    def test_requirements_property_is_parsed(self, record_property):
+        record_property("requirement", "Req21")
+
+        xml = resources.files(res).joinpath("valid_junit_with_requirements.xml")
+        graph = Graph()
+        load_junit_xml(graph, xml)
+
+        objects = list(graph.objects())
+
+        assert Literal("Req21") in objects
